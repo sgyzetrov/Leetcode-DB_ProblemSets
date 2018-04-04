162bff049d609c79207e73564c7506a840facd1b
sgyzetrov <guoyang@webmail.hzau.edu.cn>
2018-04-04 13:59:59 +0800

    modified 182.sql, removed wrong sql statement

--- 182.sql
+++ 182.sql
@@ -1,5 +1,5 @@
--- 182. (en-US_version) Find Duplicate Emails
+-- 182. Find Duplicate Emails
 select distinct Email from Person where Id not in (select min(Id) from Person group by Email )
 
--- 182. (zh-CN_version) Delete Duplicate Emails
-select * from Person where Id in (select min(Id) from Person group by Email )
+-- also
+select Email from Person Group by Email having count(Email) > 1;
